2a6c9073b115daa2f192a69d556796d1990ca648
Alexc <alexsejsi4@gmail.com>
2023-05-13 18:50:11 +0300

    Refactor mongo models

---
+++ server/src/models/mongoModels/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const configPath = path.join(__dirname, '..', '..', 'config/mongoConfig.json');
+const config = require(configPath)[env];
+
+mongoose.connect(
+  `mongodb://${config.host}:${config.port}/${config.database}`,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  }
+);
+
+const db = {};
+
+fs.readdirSync(__dirname)
+  .filter(file => {
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    );
+  })
+  .forEach(file => {
+    const model = require(path.join(__dirname, file));
+    db[model.modelName] = model;
+  });
+
+mongoose.set('debug', env === 'development');
+
+db.mongoose = mongoose;
+
+module.exports = db;
